ed51ef6bbc49bf978065eeec0001329a0c60843f
Phil Fernandez <philvfernandez@gmail.com>
2023-01-17 15:13:15 -0700

    Implemented example to consume rest services using OpenFeign.

--- src/main/java/com/easybytes/restconsume/RestconsumeApplication.java
+++ src/main/java/com/easybytes/restconsume/RestconsumeApplication.java
@@ -2,8 +2,10 @@ package com.easybytes.restconsume;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.easybytes.restconsume.proxy")
 public class RestconsumeApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/easybytes/restconsume/config/ProjectConfiguration.java
@@ -0,0 +1,14 @@
+package com.easybytes.restconsume.config;
+
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfiguration {
+
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+        return new BasicAuthRequestInterceptor("admin@eazyschool.com", "admin12345");
+    }
+}
