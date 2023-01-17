0d0de5719b22f55b78ca70df245bbac57f3ffd53
Phil Fernandez <philvfernandez@gmail.com>
2023-01-17 14:22:20 -0700

    Initial add of project.

---
+++ src/main/java/com/easybytes/restconsume/RestconsumeApplication.java
@@ -0,0 +1,13 @@
+package com.easybytes.restconsume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestconsumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestconsumeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/easybytes/restconsume/RestconsumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easybytes.restconsume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestconsumeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
